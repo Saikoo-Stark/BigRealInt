9aba10060f10a284ee8c6ced0f22ae2cb5152afe
saikoo_stark <68348213+Saikoo-Stark@users.noreply.github.com>
2022-11-14 18:43:20 +0200

    added - operator

--- BigReal.cpp
+++ BigReal.cpp
@@ -5,22 +5,8 @@
 
 bool BigReal::isValid(string num){
 
-    int dot = 0 , i = 0  ;
-
-
-    if(num[0] == '-')
-        i++;
-
-    for(; i < num.length() ; i++){
-        if(num[i] == '.')
-            dot++;
-
-        if(dot > 1 || ((num[i]-'0' > 9 || num[i]-'0' < 0) && num[i] != '.') ){
-            return 0 ;
-        }
-    }
-
-    return 1 ;
+    regex validate("[-+]?[0-9]+(['.'][0-9]*)?");
+	return regex_match(num, validate);
 }
 
 
@@ -38,6 +24,27 @@ string BigReal::str(){
     return all;
 }
 
+void BigReal::zeroRemover(BigReal &num){
+	int pos = 0;
+	for (int i = 0 ; i < num.dec.size(); i++){
+		if(num.dec[i] != '0' || i == num.dec.size()-1){
+			pos = i ;
+			break;
+		}
+	}
+	num.dec = num.dec.substr(pos);
+	
+	reverse(num.frac.begin() , num.frac.end());
+	pos = 0;
+	for (int i = 0 ; i < num.frac.size(); i++){
+		if(num.frac[i] != '0' || i == num.frac.size()-1){
+			pos = i ;
+			break;
+		}
+	}
+	num.frac = num.frac.substr(pos);
+	reverse(num.frac.begin() , num.frac.end());
+}
 
 
 BigReal::BigReal(){
@@ -285,8 +292,9 @@ ostream & operator<< (ostream& output, BigReal& num)
 
 BigReal BigReal::operator+(BigReal &other)
 {
-    BigReal result(".");
-
+    BigReal result;
+	result.dec = "";
+	result.frac = "";
     int decimalSize = max(this->dec.size(), other.dec.size());
 
     for (int i = this->dec.size(); i < decimalSize; ++i) {
@@ -356,10 +364,14 @@ BigReal BigReal::operator+(BigReal &other)
             else
             {
                 result.frac = to_string((biggerNum.frac[i] - '0') - (smallerNum.frac[i] - '0') + 10) + result.frac;
-                for (int j = i; j >= 0; --j) {
+                if(i == 0){
+                	biggerNum.dec[dec.size()-1]--;
+                }
+                for (int j = i-1; j >= 0; --j) {
                     if(biggerNum.frac[j] - '0' > 0)
                     {
                         biggerNum.frac[j] = biggerNum.frac[j] - 1;
+                        break;
                     }
                     else
                     {
@@ -376,10 +388,11 @@ BigReal BigReal::operator+(BigReal &other)
             else
             {
                 result.dec = to_string((biggerNum.dec[i] - '0') - (smallerNum.dec[i] - '0') + 10) + result.dec;
-                for (int j = i; j >= 0; --j) {
+                for (int j = i-1; j >= 0; --j) {
                     if(biggerNum.dec[j] - '0' > 0)
                     {
                         biggerNum.dec[j] = biggerNum.dec[j] - 1;
+                        break;
                     }
                     else
                     {
@@ -391,5 +404,138 @@ BigReal BigReal::operator+(BigReal &other)
 
         result.sign_ = biggerSign;
     }
+    zeroRemover(*this);
+    zeroRemover(other);
+    return result;
+}
+
+
+
+BigReal BigReal::operator-(BigReal &other)
+{
+    BigReal result;
+	result.dec = "";
+	result.frac = "";
+
+    int decimalSize = max(this->dec.size(), other.dec.size());
+
+    for (int i = this->dec.size(); i < decimalSize; ++i) {
+        this->dec = '0' + this->dec;
+    }
+    for (int i = other.dec.size(); i < decimalSize; ++i) {
+        other.dec = '0' + other.dec;
+    }
+
+    int fractionSize = max(this->frac.size(), other.frac.size());
+
+    for (int i = this->frac.size(); i < fractionSize; ++i) {
+        this->frac += '0';
+    }
+    for (int i = other.frac.size(); i < fractionSize; ++i) {
+        other.frac += '0';
+    }
+
+    string n1 = this->dec + this->frac, n2 = other.dec + other.frac;
+    char biggerSign = '~';
+    BigReal biggerNum, smallerNum;
+    for (int i = 0; i < n1.size(); ++i) {
+        if(n1[i] > n2[i])
+        {
+            biggerSign = this->sign_;
+            biggerNum = *this;
+            break;
+        }
+        else if(n2[i] > n1[i])
+        {
+            biggerSign = other.sign_;
+            biggerNum = other;
+            break;
+        }
+    }
+    if(biggerSign == '~') {
+        biggerSign = this->sign_;
+        biggerNum = *this;
+    }
+    if(biggerNum == *this)
+        smallerNum = other;
+    else
+        smallerNum = *this;
+//cout<< biggerNum.dec<<endl;
+    if(this->sign_ != other.sign_)
+    {
+        int remainder = 0;
+        for (int i = this->frac.size() - 1; i >= 0; --i) {
+            result.frac = to_string(((this->frac[i] - '0') + (other.frac[i] - '0') + remainder) % 10) + result.frac;
+            remainder = ((this->frac[i] - '0') + (other.frac[i] - '0') + remainder) / 10;
+        }
+        for (int i = this->dec.size() - 1; i >= 0; --i) {
+            result.dec = to_string(((this->dec[i] - '0') + (other.dec[i] - '0') + remainder) % 10) + result.dec;
+            remainder = ((this->dec[i] - '0') + (other.dec[i] - '0') + remainder) / 10;
+        }
+        if (remainder)
+            result.dec = to_string(remainder) + result.dec;
+        result.sign_ = this->sign_;
+    }
+    else
+    {
+        for (int i = this->frac.size() - 1; i >= 0; --i) {
+            if ((biggerNum.frac[i] - '0') - (smallerNum.frac[i] - '0') >= 0)
+            {
+                result.frac = to_string((biggerNum.frac[i] - '0') - (smallerNum.frac[i] - '0')) + result.frac;
+            }
+            else
+            {
+                result.frac = to_string((biggerNum.frac[i] - '0') - (smallerNum.frac[i] - '0') + 10) + result.frac;
+                if(i == 0){
+                	biggerNum.dec[dec.size()-1]--;
+                }
+                for (int j = i-1; j >= 0; --j) {
+                    if(biggerNum.frac[j] - '0' > 0)
+                    {
+                        biggerNum.frac[j] = biggerNum.frac[j] - 1;
+                        break;
+                    }
+                    else
+                    {
+                        biggerNum.frac[j] = '9';
+                    }
+                }
+            }
+        }
+        for (int i = this->dec.size() - 1; i >= 0; --i) {
+            if ((biggerNum.dec[i] - '0') - (smallerNum.dec[i] - '0') >= 0)
+            {
+                result.dec = to_string((biggerNum.dec[i] - '0') - (smallerNum.dec[i] - '0')) + result.dec;
+            }
+            else
+            {
+                result.dec = to_string((biggerNum.dec[i] - '0') - (smallerNum.dec[i] - '0') + 10) + result.dec;
+                for (int j = i-1; j >= 0; --j) {
+                    if(biggerNum.dec[j] - '0' > 0)
+                    {
+                        biggerNum.dec[j] = biggerNum.dec[j] - 1;
+                        break;
+                    }
+                    else
+                    {
+                        biggerNum.dec[j] = '9';
+                    }
+                }
+            }
+        }
+		//cout<< biggerNum.dec<< " " << biggerSign<<endl;
+		//cout<< n2 << " " << n1<<endl;
+		if(n2 > n1){
+		 if(biggerSign == '-')
+		      result.sign_ = '+';
+		 else if(biggerSign == '+')
+			 result.sign_ = '-';
+		}
+		else
+        result.sign_ = biggerSign;
+    }
+    
+    zeroRemover(*this);
+    zeroRemover(other);
     return result;
 }
